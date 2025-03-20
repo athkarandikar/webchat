cda70c5725be21e83f6f583c69e41ecc19ba245a
Atharva Karandikar <85398966+athkarandikar@users.noreply.github.com>
2025-03-20 16:13:37 +0530

    skip deploy

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,13 +1,13 @@
-import {initializeApp, getApps, getApp} from 'firebase/app'
-import {getFirestore} from 'firebase/firestore'
+import { initializeApp, getApps, getApp } from "firebase/app"
+import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig = {
-    apiKey: 'AIzaSyCRFVKeZBaLLM_4IOse224NYe5YTsvl1rQ',
-    authDomain: 'webchat-f730f.firebaseapp.com',
-    projectId: 'webchat-f730f',
-    storageBucket: 'webchat-f730f.appspot.com',
-    messagingSenderId: '985547033722',
-    appId: '1:985547033722:web:3f1e7595c8263a5740ffc3'
+  apiKey: "apiKey",
+  authDomain: "authDomain",
+  projectId: "projectId",
+  storageBucket: "storageBucket",
+  messagingSenderId: "messagingSenderId",
+  appId: "appId"
 }
 
 export const app = initializeApp(firebaseConfig)
